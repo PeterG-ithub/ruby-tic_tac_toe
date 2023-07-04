6609bf4aa796c91fb3a2a8a0ade0233dcd9bd81f
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-03 21:35:58 -0700

    Add play_menu and options_menu
    Play menu will ask player v player.
    Option menu will ask computer difficulty.
    Change input function to more generic.

--- main.rb
+++ main.rb
@@ -57,48 +57,104 @@ class Game
   end
 
   def intro
+    system('clear')
     puts '~~~~~~~~~~~~~~~Welcome to Tic Tac Toe!~~~~~~~~~~~~~~~'
-    puts 'To start, enter your choice:'
   end
 
-  def play
-    puts 'You picked play'
+  def play_menu
+    system('clear')
+    puts 'You picked play!'
+    puts 'Enter your choice:'
+    puts ' 1. Player v Player'
+    puts ' 2. Player v Computer'
+    puts ' 3. Main menu'
+    player_input = input(3)
+    play_input(player_input)
   end
 
-  def options
+  def options_menu
+    system('clear')
     puts 'You picked options'
+    puts 'Computer Difficulty:'
+    puts ' 1. Easy'
+    puts ' 2. Hard'
+    puts ' 3. Unbeatable'
+    puts ' 4. Main menu'
+    player_input = input(4)
+    option_input(player_input)
   end
 
   def quit
     puts 'I hope you enjoyed the game!'
+    exit
   end
 
   def main_menu
+    puts 'To start, enter your choice:'
     puts ' 1. Play'
     puts ' 2. Options'
     puts ' 3. Quit'
-    player_input = input
+    player_input = input(3)
     menu_input(player_input)
   end
 
-  def input
+  def input(num_of_choice)
     loop do
       player_input = gets.chomp
-      return player_input.to_i if player_input.to_i.between?(1, 3)
+      return player_input.to_i if player_input.to_i.between?(1, num_of_choice)
 
       puts 'Invalid choice. Please try again'
     end
   end
 
+  # Need to find a generalize code for player inputs and case statements
   def menu_input(player_input)
     case player_input.to_i
     when 1
-      play
+      play_menu
     when 2
-      options
+      options_menu
     when 3
       quit
     end
+  end
+
+  def play_input(player_input)
+    case player_input.to_i
+    when 1
+      pvp
+    when 2
+      pvc
+    when 3
+      system('clear')
+      puts 'You picked Main menu'
+      main_menu
+    end
+  end
+
+  def option_input(player_input)
+    case player_input.to_i
+    when 1..3
+      difficulty(player_input.to_i)
+      main_menu
+    when 4
+      system('clear')
+      puts 'You picked Main menu'
+      main_menu
+    end
+  end
+
+  def difficulty(difficulty)
+    system('clear')
+    case difficulty.to_i
+    when 1
+      puts 'Difficulty set to Easy'
+    when 2
+      puts 'Difficulty set to Hard'
+    when 3
+      puts 'Difficulty set to UNBEATABLE!'
+    end
+    main_menu
   end
 end
 
