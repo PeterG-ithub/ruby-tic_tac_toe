6573b01c1b2716065f149333079f298a19cb07fd
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-03 20:00:20 -0700

    Add Game class with methods to control the menu
    Added different methods to control the main menu
    of the game. Also added an input check in input method
    to check if player inputted the correct input.

--- main.rb
+++ main.rb
@@ -49,7 +49,58 @@ class Player
   end
 end
 
-board = Board.new
-board.create_board
-greg = Player.new('Greg')
-p greg.name
+# This class is for game controls
+class Game
+  def start_game
+    intro
+    main_menu
+  end
+
+  def intro
+    puts '~~~~~~~~~~~~~~~Welcome to Tic Tac Toe!~~~~~~~~~~~~~~~'
+    puts 'To start, enter your choice:'
+  end
+
+  def play
+    puts 'You picked play'
+  end
+
+  def options
+    puts 'You picked options'
+  end
+
+  def quit
+    puts 'I hope you enjoyed the game!'
+  end
+
+  def main_menu
+    puts ' 1. Play'
+    puts ' 2. Options'
+    puts ' 3. Quit'
+    player_input = input
+    menu_input(player_input)
+  end
+
+  def input
+    loop do
+      player_input = gets.chomp
+      return player_input.to_i if player_input.to_i.between?(1, 3)
+
+      puts 'Invalid choice. Please try again'
+    end
+  end
+
+  def menu_input(player_input)
+    case player_input.to_i
+    when 1
+      play
+    when 2
+      options
+    when 3
+      quit
+    end
+  end
+end
+
+game = Game.new
+game.start_game
