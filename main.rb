dfa6a202a501f148654d1a7c928754c354f448b0
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-06 18:13:52 -0700

    Add mapping to the board
    Add player name input to get the
    names of the players for the pvp

--- main.rb
+++ main.rb
@@ -1,18 +1,11 @@
 # frozen_string_literal: true
 
-require 'pry-byebug'
-# frozen_string_literal: true
-
-# puts '┏━━━┳━━━┳━━━┓'
-# puts '┃ ✖ ┃   ┃   ┃'
-# puts '┣━━━╋━━━╋━━━┫'
-# puts '┃   ┃ ● ┃   ┃'
-# puts '┣━━━╋━━━╋━━━┫'
-# puts '┃   ┃   ┃   ┃'
-# puts '┗━━━┻━━━┻━━━┛'
-
 # This class will make the boards
 class Board
+  def initialize
+    @input = Array.new(3) { Array.new(3, ' ') }
+  end
+
   def top_layer
     puts '┏━━━┳━━━┳━━━┓'
   end
@@ -21,22 +14,34 @@ class Board
     puts '┣━━━╋━━━╋━━━┫'
   end
 
-  def input_layer
-    puts '┃   ┃   ┃   ┃'
+  def input_layer(input = Array.new(3))
+    puts "┃ #{input[0]} ┃ #{input[1]} ┃ #{input[2]} ┃"
   end
 
   def bottom_layer
     puts '┗━━━┻━━━┻━━━┛'
   end
 
-  def create_board
+  def draw_board(input = @input)
     top_layer
-    input_layer
+    input_layer(input[0])
     mid_layer
-    input_layer
+    input_layer(input[1])
     mid_layer
-    input_layer
+    input_layer(input[2])
     bottom_layer
+  end
+
+  def input_logic(player_input)
+    input_mapping = {
+      'a1' => [0, 0], 'a2' => [0, 1], 'a3' => [0, 2],
+      'b1' => [1, 0], 'b2' => [1, 1], 'b3' => [1, 2],
+      'c1' => [2, 0], 'c2' => [2, 1], 'c3' => [2, 2]
+    }
+    input_arr = input_mapping[player_input]
+    row, col = input_arr
+    @input[row][col] = '✖'
+    draw_board(@input)
   end
 end
 
@@ -66,7 +71,7 @@ class Game
     puts 'You picked play!'
     puts 'Enter your choice:'
     puts ' 1. Player v Player'
-    puts ' 2. Player v Computer'
+    puts ' 2. Player v Computer (WIP)'
     puts ' 3. Main menu'
     player_input = input(3)
     play_input(player_input)
@@ -80,6 +85,7 @@ class Game
     puts ' 2. Hard'
     puts ' 3. Unbeatable'
     puts ' 4. Main menu'
+    puts 'Note: Player vs Computer not supported yet.'
     player_input = input(4)
     option_input(player_input)
   end
@@ -155,6 +161,19 @@ class Game
       puts 'Difficulty set to UNBEATABLE!'
     end
     main_menu
+  end
+
+  def pvp
+    print 'Player 1 name: '
+    player_one = Player.new(gets.chomp)
+    print 'Player 2 name: '
+    player_two = Player.new(gets.chomp)
+    puts "Player 1: #{player_one.name}"
+    puts "Player 2: #{player_two.name}"
+  end
+
+  def play_game
+
   end
 end
 
