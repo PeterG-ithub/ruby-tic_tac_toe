5dd34fb6e17287b1f7bfde823753e8075c59b933
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-07-07 15:07:05 -0700

    Add play_round and validate_input
    play_round plays one round of tictactoe.
    validate_input will check if cell is empty
    or if the user inputted the correct inputs.

--- main.rb
+++ main.rb
@@ -2,37 +2,40 @@
 
 # This class will make the boards
 class Board
+  attr_reader :input
+
   def initialize
     @input = Array.new(3) { Array.new(3, ' ') }
   end
 
   def top_layer
-    puts '┏━━━┳━━━┳━━━┓'
+    puts '    1   2   3'
+    puts '  ┏━━━┳━━━┳━━━┓'
   end
 
   def mid_layer
-    puts '┣━━━╋━━━╋━━━┫'
+    puts '  ┣━━━╋━━━╋━━━┫'
   end
 
-  def input_layer(input = Array.new(3))
-    puts "┃ #{input[0]} ┃ #{input[1]} ┃ #{input[2]} ┃"
+  def input_layer(layer_number, input = Array.new(3))
+    puts "#{layer_number} ┃ #{input[0]} ┃ #{input[1]} ┃ #{input[2]} ┃"
   end
 
   def bottom_layer
-    puts '┗━━━┻━━━┻━━━┛'
+    puts '  ┗━━━┻━━━┻━━━┛'
   end
 
   def draw_board(input = @input)
     top_layer
-    input_layer(input[0])
+    input_layer('a', input[0])
     mid_layer
-    input_layer(input[1])
+    input_layer('b', input[1])
     mid_layer
-    input_layer(input[2])
+    input_layer('c', input[2])
     bottom_layer
   end
 
-  def input_logic(player_input)
+  def input_logic(player_input, player_number)
     input_mapping = {
       'a1' => [0, 0], 'a2' => [0, 1], 'a3' => [0, 2],
       'b1' => [1, 0], 'b2' => [1, 1], 'b3' => [1, 2],
@@ -40,17 +43,34 @@ class Board
     }
     input_arr = input_mapping[player_input]
     row, col = input_arr
-    @input[row][col] = '✖'
+    @input[row][col] = '✖' if player_number == 1
+    @input[row][col] = '●' if player_number == 2
     draw_board(@input)
+  end
+
+  def isWinner?
   end
 end
 
 # This class is for player creation
 class Player
   attr_accessor :name
+  attr_reader :player_number
+
+  @player_count = 0
 
   def initialize(name)
     @name = name
+    self.class.increment_player_count
+    @player_number = self.class.player_count
+  end
+
+  def self.increment_player_count
+    @player_count += 1
+  end
+
+  class << self
+    attr_reader :player_count
   end
 end
 
@@ -130,7 +150,8 @@ class Game
     when 1
       pvp
     when 2
-      pvc
+      puts 'Doesn\'t exist'
+      main_menu
     when 3
       system('clear')
       puts 'You picked Main menu'
@@ -168,12 +189,51 @@ class Game
     player_one = Player.new(gets.chomp)
     print 'Player 2 name: '
     player_two = Player.new(gets.chomp)
-    puts "Player 1: #{player_one.name}"
-    puts "Player 2: #{player_two.name}"
+    play_round(player_one, player_two)
   end
 
-  def play_game
+  def play_round(player_one, player_two)
+    board = Board.new
+    puts 'Please use the following inputs: a1, a2, a3, b1, b2, b3, c1, c2, c3'
+    5.times do
+      print "#{player_one.name}'s turn: "
+      player1_input = validate_input(gets.chomp, board)
+      board.input_logic(player1_input.to_s, player_one.player_number)
+      if(board.isWinner?)
+        puts "#{player_one.name} Wins!"
+        quit
+      end
+      print "#{player_two.name}'s turn: "
+      player2_input = validate_input(gets.chomp, board)
+      board.input_logic(player2_input.to_s, player_two.player_number)
+      board.isWinner?
+      if(board.isWinner?)
+        puts "#{player_two.name} Wins!"
+        quit
+      end
+    end
+  end
 
+  def validate_input(string, board)
+    arr = %w[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+    input_mapping = {
+      'a1' => [0, 0], 'a2' => [0, 1], 'a3' => [0, 2],
+      'b1' => [1, 0], 'b2' => [1, 1], 'b3' => [1, 2],
+      'c1' => [2, 0], 'c2' => [2, 1], 'c3' => [2, 2]
+    }
+    loop do
+      if arr.include?(string)
+        input_arr = input_mapping[string]
+        row, col = input_arr
+        if board.input[row][col] == '✖' || board.input[row][col] == '●'
+          puts 'There is something already there :)'
+        else
+          return string
+        end
+      end
+      puts 'Please use the following inputs: a1, a2, a3, b1, b2, b3, c1, c2, c3'
+      string = gets.chomp
+    end
   end
 end
 
